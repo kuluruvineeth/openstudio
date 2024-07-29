78447832f6d056b9b7475a6a041b355af6502eda
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-07-29 11:03:13 +0530

    updated next config to config i18n plugin

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,10 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@openstudio/ui"],
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
