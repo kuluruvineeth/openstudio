91cfc63210f297b988d8b2ae5f0cd5ab0ea97fc0
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-07-29 08:24:07 +0530

    mode toggle testing in root page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 import { Alert } from "@openstudio/ui/components/ui/alert";
 import { Button } from "@openstudio/ui/components/ui/button";
 import { Badge } from "@openstudio/ui/components/ui/badge";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Page() {
   return (
     <main>
+      <ModeToggle />
       <Button>Click me</Button>
       <Alert>Hello</Alert>
       <Badge variant="outline">Badge</Badge>
