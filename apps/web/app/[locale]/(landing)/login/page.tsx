72c2053c04fa18d2dc549ee4f270e735f577a044
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-07-29 15:05:00 +0530

    added theme switcher and locale switcher to login page

--- apps/web/app/[locale]/(landing)/login/page.tsx
+++ apps/web/app/[locale]/(landing)/login/page.tsx
@@ -3,6 +3,8 @@ import { Suspense } from "react";
 import { LoginForm } from "@/app/[locale]/(landing)/login/_components/LoginForm";
 import LoginHeader from "@/app/[locale]/(landing)/login/_components/LoginHeader";
 import LoginFooter from "./_components/LoginFooter";
+import { ThemeSwitcher } from "@/components/switchers/ThemeSwitcher";
+import { LocaleSwitcher } from "@/components/switchers/LocaleSwitcher";
 
 export const metadata: Metadata = {
   title: "Log in | Open Studio",
@@ -15,7 +17,13 @@ export const metadata: Metadata = {
 export default async function LoginPage() {
   return (
     <Suspense>
-      <div className="flex h-screen flex-col justify-center text-gray-900">
+      <main className="flex flex-col gap-3 justify-center items-center min-h-screen w-full p-4 md:p-6">
+        <div className="absolute top-0 left-0 w-full flex justify-end">
+          <div className="flex items-center gap-2 max-w-7xl p-4 md:p-6">
+            <LocaleSwitcher />
+            <ThemeSwitcher />
+          </div>
+        </div>
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <LoginHeader />
           <div className="mt-4">
@@ -25,7 +33,7 @@ export default async function LoginPage() {
           </div>
           <LoginFooter />
         </div>
-      </div>
+      </main>
     </Suspense>
   );
 }
