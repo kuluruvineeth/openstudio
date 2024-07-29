87c9cd24992a38031b912f9d5ebff80aace7d978
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-07-29 15:07:00 +0530

    added tentative logout logic to test functionality

--- apps/web/app/[locale]/(app)/dashboard/page.tsx
+++ apps/web/app/[locale]/(app)/dashboard/page.tsx
@@ -1,17 +1,28 @@
+"use client";
 import { Alert } from "@openstudio/ui/components/ui/alert";
 import { Button } from "@openstudio/ui/components/ui/button";
 import { Badge } from "@openstudio/ui/components/ui/badge";
-import { ModeToggle } from "@/components/mode-toggle";
 import { useTranslations } from "next-intl";
+import { supabaseBrowserClient } from "@/supabase/supabaseClient";
+import { useRouter } from "next/navigation";
+import { toastSuccess } from "@openstudio/ui/components/Toast";
 
 export default function Page() {
   const t = useTranslations("Index");
+  const router = useRouter();
+
+  const onLogout = async () => {
+    await supabaseBrowserClient.auth.signOut();
+    toastSuccess({
+      title: "Logout",
+      description: "You have been logged out",
+    });
+    router.push("/");
+  };
 
   return (
     <main>
-      <h1>{t("title")}</h1>;
-      <ModeToggle />
-      <Button>Dashboard</Button>
+      <h1>{t("title")}</h1>;<Button onClick={onLogout}>Log Out</Button>
       <Alert>Dashboard</Alert>
       <Badge variant="outline">Dashboard</Badge>
     </main>
