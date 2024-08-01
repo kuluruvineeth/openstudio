5b1bf39e8b5f8cac7fd6140cb49769e25b500e14
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-01 09:02:06 +0530

    added container for sidebar wrapping sidebar content

---
+++ apps/web/components/sidebar/SidebarContainer.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useToggleSidebar } from "@/context/ToggleSidebar";
+import { ShortcutSidebar } from "@/components/sidebar/shortcutSidebar/ShortcutSidebar";
+import { CloseSidebar } from "@/components/sidebar/CloseSidebar";
+
+interface SidebarContainerProps {}
+
+export const SidebarContainer = ({}: SidebarContainerProps) => {
+  const { isSidebarOpen, setIsSidebarOpen } = useToggleSidebar();
+
+  return (
+    <>
+      <aside
+        className={`fixed z-50 top-0 left-0 lg:static h-full bg-background border-r flex lg:translate-x-0 transition-all duration-300 ${
+          isSidebarOpen ? "translate-x-0 shadow-sm" : "translate-x-[-100%]"
+        }`}
+      >
+        <ShortcutSidebar />
+        <CloseSidebar />
+      </aside>
+      <div
+        onClick={() => setIsSidebarOpen(false)}
+        className={`fixed h-screen w-full top-0 left-0 bg-black/80 z-40 lg:hidden ${
+          isSidebarOpen ? "block" : "hidden"
+        }`}
+      ></div>
+    </>
+  );
+};
