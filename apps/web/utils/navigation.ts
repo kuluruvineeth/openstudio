ec67bf77afaa19c2c433b4ec8d4ca9d02ca56e59
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-01 08:35:32 +0530

    destructured Link, redirect and usePathname from next-intl navigation

--- apps/web/utils/navigation.ts
+++ apps/web/utils/navigation.ts
@@ -2,8 +2,9 @@ import { createLocalizedPathnamesNavigation } from "next-intl/navigation";
 import { locales, pathnames, localePrefix } from "./config";
 
 //TODO: fix type error
-export const { usePathname, useRouter } = createLocalizedPathnamesNavigation({
-  locales,
-  pathnames,
-  localePrefix,
-});
+export const { Link, getPathname, redirect, usePathname, useRouter } =
+  createLocalizedPathnamesNavigation({
+    locales,
+    pathnames,
+    localePrefix,
+  });
