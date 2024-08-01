77d978a60ccdc6ef7416bf956fa0821a410cfbe2
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-01 09:00:42 +0530

    added shortcut sidebar component

---
+++ apps/web/components/sidebar/shortcutSidebar/ShortcutSidebar.tsx
@@ -0,0 +1,16 @@
+import { ScrollArea } from "@openstudio/ui/components/ui/scroll-area";
+import { Top } from "@/components/sidebar/shortcutSidebar/Top";
+
+interface ShortcutSidebarProps {}
+
+export const ShortcutSidebar = ({}: ShortcutSidebarProps) => {
+  return (
+    <div className="flex flex-col justify-between items-center h-full p-1 sm:py-6 border-r gap-2">
+      <ScrollArea className="max-h-[35rem]">
+        <div className="w-full space-y-3 p-1">
+          <Top />
+        </div>
+      </ScrollArea>
+    </div>
+  );
+};
