2bec6e4aa6dde400f3e069b6d133329fa10404a2
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-01 09:34:10 +0530

    added get user data action to fetch user details

---
+++ apps/web/actions/GetUserData.ts
@@ -0,0 +1,27 @@
+import { supabaseServerClient } from "@/supabase/supabaseServer";
+import { User } from "@/types/app";
+
+export const getUserData = async (): Promise<User | null> => {
+  const supabase = await supabaseServerClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    console.log("NO USER", user);
+    return null;
+  }
+
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("id", user.id);
+
+  if (error) {
+    console.log(error);
+    return null;
+  }
+
+  return data ? data[0] : null;
+};
