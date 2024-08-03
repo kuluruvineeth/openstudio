605814298db155adad9ae0dd82af305c7893470d
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-03 06:05:30 +0530

    created options sidebar component to wrap settings and other components

---
+++ apps/web/components/sidebar/optionsSidebar/OptionsSidebar.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { usePathname } from "@/utils/navigation";
+import { ScrollArea } from "@openstudio/ui/components/ui/scroll-area";
+import { Settings } from "@/components/sidebar/optionsSidebar/settingsOptions/Settings";
+
+interface OptionsSidebarProps {}
+
+export const OptionsSidebar = ({}: OptionsSidebarProps) => {
+  const pathname = usePathname();
+
+  if (pathname === "/dashboard") {
+    return null;
+  }
+
+  return (
+    <div className="h-full p-4 sm:py-6 border-r w-52 sm:w-64 flex flex-col justify-between gap-2">
+      <ScrollArea className="h-full">
+        {pathname.includes("/dashboard/settings") && <Settings />}
+      </ScrollArea>
+    </div>
+  );
+};
