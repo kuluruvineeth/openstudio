45d482213e37906adfedad818a77a114cf6b3a8a
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-03 06:03:23 +0530

    added interface and constants for themes

--- apps/web/utils/constants.ts
+++ apps/web/utils/constants.ts
@@ -14,3 +14,27 @@ export const TOP_SIDEBAR_LINKS: TopSidebarLink[] = [
     hoverTextKey: "HOME_HOVER",
   },
 ];
+
+export interface Themes {
+  type: "light" | "dark" | "system";
+  title: string;
+  footer: string;
+}
+
+export const THEMES: Themes[] = [
+  {
+    type: "light",
+    title: "THEME.LIGHT_TITLE",
+    footer: "THEME.LIGHT_FOOTER",
+  },
+  {
+    type: "dark",
+    title: "THEME.DARK_TITLE",
+    footer: "THEME.DARK_FOOTER",
+  },
+  {
+    type: "system",
+    title: "THEME.SYSTEM_TITLE",
+    footer: "THEME.SYSTEM_FOOTER",
+  },
+];
