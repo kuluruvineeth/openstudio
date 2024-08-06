e5f343427ac0982be1bc057dc258fe1c0b168b23
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-06 10:27:47 +0530

    created reusable auth util function

---
+++ apps/web/utils/auth.ts
@@ -0,0 +1,20 @@
+import { getUserData } from "@/actions/GetUserData";
+import { supabaseServerClient } from "@/supabase/supabaseServer";
+
+export const auth = async () => {
+  const { error } = await (await supabaseServerClient()).auth.getSession();
+  if (error) {
+    return null;
+  }
+  const userData = await getUserData();
+
+  if (!userData || !userData.access_token || !userData.refresh_token) {
+    return null;
+  }
+
+  return {
+    ownerEmail: userData.email,
+    accessToken: userData?.access_token,
+    refreshToken: userData?.refresh_token,
+  };
+};
