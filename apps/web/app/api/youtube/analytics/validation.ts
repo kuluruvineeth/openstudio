e940c57737533cd5d4e8f6501b5f5f476e0ac36e
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-06 11:27:19 +0530

    added zod validating analytics query object

---
+++ apps/web/app/api/youtube/analytics/validation.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const zodDimension = z.enum(["day", "week", "month", "year"]);
+
+export const analyticsQuery = z.object({
+  fromDate: z.string(),
+  toDate: z.string(),
+  dimensions: zodDimension,
+  type: z.enum(["channel", "subscribers"]),
+  metrics: z
+    .enum([
+      "views",
+      "subscribersGained",
+      "subscribersLost",
+      "likes",
+      "dislikes",
+      "comments",
+      "shares",
+    ])
+    .array(),
+});
+
+export type AnalyticsQuery = z.infer<typeof analyticsQuery>;
