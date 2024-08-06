626984c65c9bcbab58ee18ebe8c7c633f4a63da5
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-06 09:43:01 +0530

    added skeleton component

---
+++ packages/ui/src/components/ui/skeleton.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@openstudio/ui/lib/utils"
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  )
+}
+
+export { Skeleton }
