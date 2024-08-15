b50e182b073fe067b606960550eb945f334ee855
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-15 11:13:07 +0530

    Added `CommentItem` interface for comment data structure

---
+++ apps/web/types/youtube/comment.ts
@@ -0,0 +1,11 @@
+export interface CommentItem {
+  videoId: string;
+  videoThumbnail: string;
+  videoTitle: string;
+  videoDescription: string;
+  commentId: string | null | undefined;
+  commentedAt: string | null | undefined;
+  commentedText: string | null | undefined;
+  authorDisplayName: string | null | undefined;
+  authorProfileImageUrl: string | null | undefined;
+}
