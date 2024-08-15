5e35e2fe8449bfe5c7feb4e57e21d29c75f900ac
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-15 11:01:23 +0530

    Implemented function to delete records from Tinybird datasource.

---
+++ packages/tinybird/src/delete.ts
@@ -0,0 +1,44 @@
+const TINYBIRD_BASE_URL = process.env.TINYBIRD_BASE_URL;
+const TINYBIRD_TOKEN = process.env.TINYBIRD_TOKEN;
+
+if (!TINYBIRD_BASE_URL || !TINYBIRD_TOKEN) {
+  throw new Error("Missing TINYBIRD_TOKEN");
+}
+
+async function deleteFromDatasource(
+  datasource: string,
+  deleteCondition: string, // eg. "ownerEmail='abc@gmail.com'"
+): Promise<unknown> {
+  const url = new URL(
+    `/v0/datasources/${datasource}/delete`,
+    TINYBIRD_BASE_URL,
+  );
+
+  let res = await fetch(url, {
+    method: "POST",
+    body: `delete_condition=(${deleteCondition})`,
+    headers: {
+      Authorization: `Bearer ${TINYBIRD_TOKEN}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(
+      `Unable to delete for datasource ${datasource}: [${
+        res.status
+      }] ${await res.text()}`,
+    );
+  }
+
+  return await res.json();
+}
+
+export async function deleteTinybirdComments(options: {
+  ownerEmail: string;
+}): Promise<unknown> {
+  return await deleteFromDatasource(
+    "comment",
+    `ownerEmail='${options.ownerEmail}`,
+  );
+}
