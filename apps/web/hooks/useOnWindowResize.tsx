f36de7cbdf752bac102a150e669c3726cff538c3
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-15 10:19:53 +0530

    added useOnWindowResize hook to handle window resize events

---
+++ apps/web/hooks/useOnWindowResize.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export const useOnWindowResize = (handler: { (): void }) => {
+  React.useEffect(() => {
+    const handleResize = () => {
+      handler();
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handler]);
+};
