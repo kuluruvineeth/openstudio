8fcded5807840cb26e45928d2b68813f9ca8bd2b
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-16 15:38:26 +0530

    disabled react strict mode

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@openstudio/ui", "@t3-oss/env-nextjs"],
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
