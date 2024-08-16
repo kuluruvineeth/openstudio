eaa4ab153096449c543cc4020f7a01819d7c33f2
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-16 15:39:14 +0530

    Added type definitions and type guard functions for error handling

---
+++ apps/web/utils/error.ts
@@ -0,0 +1,13 @@
+export type ErrorMessage = { error: string; data?: any };
+
+export type ZodError = {
+  error: { issues: { code: string; message: string }[] };
+};
+
+export function isError(value: any): value is ErrorMessage | ZodError {
+  return value?.error;
+}
+
+export function isErrorMessage(value: any): value is ErrorMessage {
+  return typeof value?.error === "string";
+}
