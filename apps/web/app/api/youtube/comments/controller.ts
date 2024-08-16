e730c036f3f0789451579425c47af52facf8ffcc
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-16 15:53:00 +0530

    Completely refactored to get comments in better way

--- apps/web/app/api/youtube/comments/controller.ts
+++ apps/web/app/api/youtube/comments/controller.ts
@@ -1,89 +1,140 @@
 import { auth } from "@/utils/auth";
 import { getYoutubeDataClient } from "@/utils/youtube/client";
 import { youtube_v3 } from "googleapis";
-import Schema$PlaylistItem = youtube_v3.Schema$PlaylistItem;
-import Schema$CommentThread = youtube_v3.Schema$CommentThread;
-import { Thumbnails, VideoItem } from "@/types/youtube/playlistItems";
+import { VideoItem } from "@/types/youtube/playlistItems";
 import { CommentItem } from "@/types/youtube/comment";
+import { getCommentNextPageToken } from "@/actions/GetCommentNextPageToken";
+import { storeCommentNextPageToken } from "@/actions/StoreCommentNextPageToken";
+import { getLatestOrOldestComment } from "@openstudio/tinybird";
 
-// Fetch all videos from a channel
-async function getAllVideosFromPlaylist(
-  channelId: string,
+async function processCommentsWithVideoDetails(
+  commentThreads: youtube_v3.Schema$CommentThread[],
   youtubeDataClient: youtube_v3.Youtube,
-): Promise<VideoItem[]> {
-  try {
-    let videos: VideoItem[] = [];
-    let nextPageToken = null;
+): Promise<CommentItem[]> {
+  const videoIds = extractVideoIds(commentThreads);
+  const videoDetails = await getVideoDetailsBulk(videoIds, youtubeDataClient);
+  const videoDetailsMap = new Map(
+    videoDetails.map((video) => [video.id, video]),
+  );
 
-    do {
-      const response: any = await youtubeDataClient.playlistItems.list({
-        part: ["snippet", "contentDetails", "id", "status"],
-        playlistId: channelId, // channelId gives me all videos of including playlists
-        // maxResults: 5, //TODO: Need to pass in param this later
-        pageToken: nextPageToken !== null ? nextPageToken : undefined,
-      });
-
-      const playlistItems: Schema$PlaylistItem[] = response.data.items || [];
-      playlistItems.forEach((item) => {
-        if (item.snippet && item.status?.privacyStatus !== "private") {
-          videos.push({
-            id: item.contentDetails?.videoId ?? "N/A",
-            title: item.snippet.title ?? "No Title",
-            description: item.snippet.description ?? "No Description",
-            thumbnails: item.snippet.thumbnails as Thumbnails,
-          });
-        }
-      });
-      nextPageToken = response.data.nextPageToken;
-    } while (nextPageToken); //For now restricting to 5 videos for development purposes
-    return videos;
-  } catch (error) {
-    return [];
-  }
+  return commentThreads.map((item) => {
+    const videoId = item.snippet?.topLevelComment?.snippet?.videoId;
+    const video = videoId ? videoDetailsMap.get(videoId) : undefined;
+    return {
+      videoId: videoId ?? "N/A",
+      videoThumbnail: video?.thumbnails?.default?.url ?? "N/A",
+      videoTitle: video?.title ?? "No Title",
+      videoDescription: video?.description ?? "No Description",
+      commentId: item.id,
+      commentedAt: item.snippet?.topLevelComment?.snippet?.publishedAt,
+      commentedText: item.snippet?.topLevelComment?.snippet?.textDisplay,
+      authorDisplayName:
+        item.snippet?.topLevelComment?.snippet?.authorDisplayName,
+      authorProfileImageUrl:
+        item.snippet?.topLevelComment?.snippet?.authorProfileImageUrl,
+    };
+  });
 }
 
-async function getCommentsByVideoId(
-  videoItem: VideoItem,
+function extractVideoIds(
+  commentThreads: youtube_v3.Schema$CommentThread[],
+): string[] {
+  return [
+    ...new Set(
+      commentThreads
+        .map((thread) => thread.snippet?.topLevelComment?.snippet?.videoId)
+        .filter(Boolean),
+    ),
+  ] as string[];
+}
+
+async function fetchLatestComments(
+  ownerEmail: string,
   youtubeDataClient: youtube_v3.Youtube,
-  resultsPerPage: number = 20,
-): Promise<CommentItem[]> {
-  let videoComments: CommentItem[] = [];
-  let nextPageToken = null;
-  try {
-    do {
-      const response: any = await youtubeDataClient.commentThreads.list({
-        videoId: videoItem.id,
-        part: ["id", "replies", "snippet"],
-        pageToken: nextPageToken !== null ? nextPageToken : undefined,
-        maxResults: resultsPerPage,
-      });
+  channelId: string,
+): Promise<{ comments: CommentItem[]; nextPageToken?: string }> {
+  const latestCommentsResponse = await youtubeDataClient.commentThreads.list({
+    allThreadsRelatedToChannelId: channelId,
+    part: ["id", "replies", "snippet"],
+    maxResults: 100,
+  });
 
-      const commentThreads: Schema$CommentThread[] = response.data.items || [];
+  const [newestCommentSaved] = await Promise.all([
+    getLatestOrOldestComment({
+      ownerEmail: ownerEmail,
+      direction: "newest",
+    }),
+  ]);
 
-      commentThreads.forEach((item) => {
-        videoComments.push({
-          videoId: videoItem.id,
-          videoThumbnail: videoItem.thumbnails.default.url,
-          videoTitle: videoItem.title,
-          videoDescription: videoItem.description,
-          commentId: item.id,
-          commentedAt: item.snippet?.topLevelComment?.snippet?.publishedAt,
-          commentedText: item.snippet?.topLevelComment?.snippet?.textDisplay,
-          authorDisplayName:
-            item.snippet?.topLevelComment?.snippet?.authorDisplayName,
-          authorProfileImageUrl:
-            item.snippet?.topLevelComment?.snippet?.authorProfileImageUrl,
-        });
-      });
-      nextPageToken = response.data.nextPageToken;
-    } while (nextPageToken);
-    return videoComments;
-  } catch (error) {
-    console.error(
-      `Error fetching comments for video : ${videoItem.id} : , ${error}`,
+  const latestComments = latestCommentsResponse.data.items || [];
+
+  // Filter out comments that are older than or equal to the latest known timestamp
+  const filteredComments = newestCommentSaved.data[0]?.timestamp
+    ? latestComments.filter((comment: any) => {
+        const commentTimestamp = new Date(
+          comment.snippet?.topLevelComment?.snippet?.publishedAt,
+        ).getTime();
+        return commentTimestamp > newestCommentSaved.data[0]?.timestamp!;
+      })
+    : latestComments;
+
+  let comments: CommentItem[] = [];
+  if (filteredComments.length > 0) {
+    comments = await processCommentsWithVideoDetails(
+      filteredComments,
+      youtubeDataClient,
     );
-    throw error;
   }
+
+  return {
+    comments,
+    nextPageToken: latestCommentsResponse.data.nextPageToken ?? undefined,
+  };
+}
+
+// Helper function to fetch video details in bulk
+async function getVideoDetailsBulk(
+  videoIds: string[],
+  youtubeDataClient: youtube_v3.Youtube,
+): Promise<VideoItem[]> {
+  const response: any = await youtubeDataClient.videos.list({
+    part: ["snippet"],
+    id: videoIds,
+  });
+  return (
+    response.data.items?.map((video: any) => ({
+      id: video.id!,
+      title: video.snippet?.title ?? "No Title",
+      description: video.snippet?.description ?? "No Description",
+      thumbnails: video.snippet?.thumbnails ?? {},
+    })) || []
+  );
+}
+
+// Helper function to fetch comments with video details
+async function fetchCommentsWithVideoDetails(
+  youtubeDataClient: youtube_v3.Youtube,
+  channelId: string,
+  pageToken?: string,
+): Promise<{ comments: CommentItem[]; nextPageToken?: string }> {
+  const response = await youtubeDataClient.commentThreads.list({
+    allThreadsRelatedToChannelId: channelId,
+    part: ["id", "replies", "snippet"],
+    pageToken: pageToken ?? undefined,
+    maxResults: 100, // Adjust as needed
+  });
+
+  const commentThreads = response.data.items || [];
+
+  const comments = await processCommentsWithVideoDetails(
+    commentThreads,
+    youtubeDataClient,
+  );
+
+  return {
+    comments,
+    nextPageToken: response.data.nextPageToken ?? undefined,
+  };
 }
 
 export async function getComments(): Promise<CommentItem[]> {
@@ -102,26 +153,41 @@ export async function getComments(): Promise<CommentItem[]> {
       mine: true,
     });
     if (response.status === 200 && response.data.items) {
-      console.log(response.data.items[0]);
-      const videos = await getAllVideosFromPlaylist(
-        response.data.items[0]?.contentDetails?.relatedPlaylists
-          ?.uploads as string,
+      const channelId = response.data.items[0]?.id as string;
+      // Retrieve the stored page token if available
+      const pageInfo = await getCommentNextPageToken(channelId);
+
+      const latestCommentsResponse = await fetchLatestComments(
+        session.ownerEmail,
         youtubeDataClient,
+        channelId,
       );
+      const latestComments = latestCommentsResponse.comments;
+      if (pageInfo && pageInfo?.noMoreComments) return latestComments;
+      let paginatedComments: CommentItem[] = [];
+      let nextPageToken = latestCommentsResponse.nextPageToken;
+      if (pageInfo?.nextPageToken) {
+        const { comments, nextPageToken: paginatedToken } =
+          await fetchCommentsWithVideoDetails(
+            youtubeDataClient,
+            channelId,
+            pageInfo?.nextPageToken,
+          );
+        paginatedComments = comments;
+        nextPageToken = paginatedToken;
+      }
 
-      // Use Promise.all to wait for all comments to be fetched
-      const commentPromises = videos.map(async (item) => {
-        return await getCommentsByVideoId(item, youtubeDataClient, 20);
-      });
+      comments = latestComments.concat(paginatedComments);
 
-      // Wait for all promises to resolve and flatten the array
-      comments = (await Promise.all(commentPromises)).flat();
+      // Update the stored page token
+      if (nextPageToken) {
+        await storeCommentNextPageToken(channelId, nextPageToken);
+      } else {
+        //TODO: Clear the stored token if there are no more pages
+        await storeCommentNextPageToken(channelId, null, true);
+      }
     }
-    // Sort comments by commentedAt in descending order
-    return comments.sort(
-      (a, b) =>
-        new Date(b.commentedAt!).getTime() - new Date(a.commentedAt!).getTime(),
-    );
+    return comments;
   } catch (error) {
     console.log(`Error fetching comments : ${error}`);
     throw error;
