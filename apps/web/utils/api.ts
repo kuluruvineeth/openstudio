d4cf7f453df63d848695bfdbf089f706d015bbae
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-16 15:55:12 +0530

    Add generic `postRequest` and `getRequest` methods with error handling

--- apps/web/utils/api.ts
+++ apps/web/utils/api.ts
@@ -1,5 +1,41 @@
 import axios from "axios";
 
+import { ErrorMessage, isErrorMessage } from "./error";
+
 export const api = axios.create({
   baseURL: "/api",
 });
+
+export async function postRequest<T, S = any>(
+  url: string,
+  data: S,
+): Promise<T | ErrorMessage> {
+  try {
+    const response = await api.post<T>(url, data, {
+      headers: { "Content-Type": "application/json" },
+    });
+    return response.data;
+  } catch (error) {
+    if (isErrorMessage(error)) {
+      if (error.error === "Failed to fetch" || !navigator.onLine) {
+        return {
+          error: "Please check that you are connected to the Internet.",
+        };
+      }
+      return error;
+    }
+    return { error: "An error occurred" };
+  }
+}
+
+export async function getRequest<T>(url: string): Promise<T | ErrorMessage> {
+  try {
+    const response = await api.get<T>(url);
+    return response.data;
+  } catch (error) {
+    if (isErrorMessage(error)) {
+      return error;
+    }
+    return { error: "An error occurred" };
+  }
+}
