903b49cb6cdb8867d7b3d650b001e223dcb35545
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-16 15:47:32 +0530

    deleted unused comments body zod validation

--- apps/web/app/api/user/stats/tinybird/load/validation.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const loadTinybirdCommentsBody = z.object({
-  loadBefore: z.coerce.boolean().optional(),
-});
-
-export type LoadTinybirdCommentsBody = z.infer<typeof loadTinybirdCommentsBody>;
