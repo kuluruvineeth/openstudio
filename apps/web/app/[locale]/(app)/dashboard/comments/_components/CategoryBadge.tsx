fbb69ba88554e397aaa813f0d1b6ae3c2b8b842b
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-20 07:45:54 +0530

    Added `CategoryBadge` component to display badges with colors based on comment category.

--- apps/web/app/[locale]/(app)/dashboard/comments/_components/CategoryBadge.tsx
+++ apps/web/app/[locale]/(app)/dashboard/comments/_components/CategoryBadge.tsx
@@ -1,3 +1,6 @@
+import { Badge, Color } from "@openstudio/ui/components/Badge";
+import { capitalCase } from "change-case";
+
 const categoryColors: Record<string, Color> = {
   POSITIVE: "green",
   NEGATIVE: "red",
@@ -16,3 +19,16 @@ const categoryColors: Record<string, Color> = {
   PERSONAL_STORY: "violet",
   OTHER: "gray",
 };
+
+export function CategoryBadge(props: { category?: string }) {
+  const { category } = props;
+
+  return (
+    <Badge
+      color={category ? categoryColors[category] || "gray" : "gray"}
+      className="max-w-[100px] overflow-hidden"
+    >
+      {capitalCase(category || "Uncategorized")}
+    </Badge>
+  );
+}
