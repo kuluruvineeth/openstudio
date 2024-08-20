7074db4a2efa03683e20850d75bb17e264c842af
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-20 07:50:54 +0530

    Defined types and utilities for action errors and server responses, including `ActionError`, `ServerActionResponse`, and `SafeError`.

--- apps/web/utils/error.ts
+++ apps/web/utils/error.ts
@@ -11,3 +11,24 @@ export function isError(value: any): value is ErrorMessage | ZodError {
 export function isErrorMessage(value: any): value is ErrorMessage {
   return typeof value?.error === "string";
 }
+
+export type ActionError<T = {}> = { error: string } & T;
+export type ServerActionResponse<T = {}, S = {}> =
+  | ActionError<S>
+  | T
+  | undefined;
+
+export function isActionError(error: any): error is ActionError {
+  return error && "error" in error && error.error;
+}
+
+// This class is used to throw error messages that are safe to expose to the client.
+export class SafeError extends Error {
+  constructor(
+    public safeMessage: string,
+    message?: string,
+  ) {
+    super(message || safeMessage);
+    this.name = "SafeError";
+  }
+}
