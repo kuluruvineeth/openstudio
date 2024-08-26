f1f63fe56b140f0cd07efdc62cd8fb612cbeb979
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-26 18:28:04 +0530

    deleted legacy code

--- apps/web/app/[locale]/(app)/dashboard/comments/_components/CommentDate.tsx
+++
@@ -1,9 +0,0 @@
-import { formatShortDate } from "@/utils/date";
-
-export function CommentDate(props: { date: Date }) {
-  return (
-    <div className="flex-shrink-0 text-sm font-medium leading-5 text-gray-500">
-      {formatShortDate(props.date, { includeYear: true, lowerCase: true })}
-    </div>
-  );
-}
