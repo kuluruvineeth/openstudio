e8d0d035b8ab28fbb17a001f99dacb131736e9a4
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-26 07:38:35 +0530

    Added `SectionHeader` component for displaying a title and description

---
+++ apps/web/app/[locale]/(dashboard)/dashboard/comments/authors/_components/SectionHeader.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Text, Title } from "@tremor/react";
+
+export function SectionHeader(props: { title: string; description: string }) {
+  return (
+    <div>
+      <Title>{props.title}</Title>
+      <Text className="mt-2">{props.description}</Text>
+    </div>
+  );
+}
