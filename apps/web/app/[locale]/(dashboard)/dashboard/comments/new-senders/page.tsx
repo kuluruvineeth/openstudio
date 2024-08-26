378fb18844a1b4c92bb923a1314dab046f52036f
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-26 07:54:49 +0530

    Added NewSenders Page to display NewSenders

---
+++ apps/web/app/[locale]/(dashboard)/dashboard/comments/new-senders/page.tsx
@@ -0,0 +1,11 @@
+import { NewSenders } from "./_components/NewSenders";
+
+export default function NewSendersPage() {
+  return (
+    <div>
+      <div className="m-4">
+        <NewSenders />
+      </div>
+    </div>
+  );
+}
