c27f7066279aefd4377bc88d3cfa733dc1f9b3fc
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-26 19:08:21 +0530

    Add YouTubeVideo component for embedding YouTube videos with customizable options

---
+++ packages/ui/src/components/YoutubeVideo.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import YouTube from "react-youtube";
+import { cn } from "../lib/utils";
+
+export function YouTubeVideo(props: {
+  videoId: string;
+  iframeClassName?: string;
+  className?: string;
+  opts?: {
+    height?: string;
+    width?: string;
+    playerVars?: {
+      autoplay?: number;
+    };
+  };
+}) {
+  return (
+    <YouTube
+      videoId={props.videoId}
+      className={cn("aspect-video h-full w-full rounded-lg", props.className)}
+      iframeClassName={props.iframeClassName}
+      opts={props.opts}
+    />
+  );
+}
