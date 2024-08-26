8cf13e5d7662dd3ed3d9ee0c2584d999d1ed97b0
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-26 18:18:47 +0530

    Added query parameter support to useComments hook for fetching YouTube comments

--- apps/web/hooks/youtube/useComments.tsx
+++ apps/web/hooks/youtube/useComments.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { CommentStatsQuery } from "@/app/api/user/stats/comments/validation";
+import { CommentsQuery } from "@/app/api/youtube/comments/tinybird/validation";
 import { api } from "@/utils/api";
 
-export const useComments = async () => {
+export const useComments = async (
+  query?: CommentStatsQuery | CommentsQuery,
+) => {
   //TODO: fix type specific
-  const { data } = await api.get<any>("/youtube/comments/tinybird", {});
+  const { data } = await api.get<any>("/youtube/comments/tinybird", {
+    params: {
+      ...query,
+    },
+  });
 
   return { allComments: data.allComments };
 };
