3d5bb1c43ba1a11af27e659ad90d3d1a3acac28c
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-26 19:54:14 +0530

    pulled supabase db types

--- apps/web/types/db.ts
+++ apps/web/types/db.ts
@@ -9,6 +9,76 @@ export type Json =
 export type Database = {
   public: {
     Tables: {
+      action: {
+        Row: {
+          created_at: string | null
+          id: string
+          reply_prompt: string | null
+          rule_id: string
+          type: Database["public"]["Enums"]["action_type"]
+          updated_at: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: string
+          reply_prompt?: string | null
+          rule_id: string
+          type: Database["public"]["Enums"]["action_type"]
+          updated_at?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: string
+          reply_prompt?: string | null
+          rule_id?: string
+          type?: Database["public"]["Enums"]["action_type"]
+          updated_at?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "fk_rule"
+            columns: ["rule_id"]
+            isOneToOne: false
+            referencedRelation: "rule"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      author: {
+        Row: {
+          created_at: string | null
+          display_name: string
+          id: string
+          status: Database["public"]["Enums"]["author_status"] | null
+          updated_at: string | null
+          user_id: string
+        }
+        Insert: {
+          created_at?: string | null
+          display_name: string
+          id?: string
+          status?: Database["public"]["Enums"]["author_status"] | null
+          updated_at?: string | null
+          user_id: string
+        }
+        Update: {
+          created_at?: string | null
+          display_name?: string
+          id?: string
+          status?: Database["public"]["Enums"]["author_status"] | null
+          updated_at?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "fk_user"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       channel_comment_pagination: {
         Row: {
           channel_id: string
@@ -30,47 +100,322 @@ export type Database = {
         }
         Relationships: []
       }
+      executed_action: {
+        Row: {
+          created_at: string | null
+          executed_rule_id: string
+          id: string
+          reply: string | null
+          type: Database["public"]["Enums"]["action_type"]
+          updated_at: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          executed_rule_id: string
+          id?: string
+          reply?: string | null
+          type: Database["public"]["Enums"]["action_type"]
+          updated_at?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          executed_rule_id?: string
+          id?: string
+          reply?: string | null
+          type?: Database["public"]["Enums"]["action_type"]
+          updated_at?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "fk_executed_rule"
+            columns: ["executed_rule_id"]
+            isOneToOne: false
+            referencedRelation: "executed_rule"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      executed_rule: {
+        Row: {
+          automated: boolean
+          comment_id: string
+          created_at: string | null
+          id: string
+          reason: string | null
+          rule_id: string | null
+          status: Database["public"]["Enums"]["executed_rule_status"] | null
+          updated_at: string | null
+          user_id: string
+          video_id: string
+        }
+        Insert: {
+          automated: boolean
+          comment_id: string
+          created_at?: string | null
+          id?: string
+          reason?: string | null
+          rule_id?: string | null
+          status?: Database["public"]["Enums"]["executed_rule_status"] | null
+          updated_at?: string | null
+          user_id: string
+          video_id: string
+        }
+        Update: {
+          automated?: boolean
+          comment_id?: string
+          created_at?: string | null
+          id?: string
+          reason?: string | null
+          rule_id?: string | null
+          status?: Database["public"]["Enums"]["executed_rule_status"] | null
+          updated_at?: string | null
+          user_id?: string
+          video_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "fk_rule"
+            columns: ["rule_id"]
+            isOneToOne: false
+            referencedRelation: "rule"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "fk_user"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      premium: {
+        Row: {
+          ai_automation_access:
+            | Database["public"]["Enums"]["feature_access"]
+            | null
+          ai_credits: number | null
+          ai_month: number | null
+          approve_or_ban_credits: number | null
+          approve_or_ban_month: number | null
+          bulk_approve_or_ban_access:
+            | Database["public"]["Enums"]["feature_access"]
+            | null
+          created_at: string | null
+          id: string
+          lemon_license_instance_id: string | null
+          lemon_license_key: string | null
+          lemon_squeezy_customer_id: number | null
+          lemon_squeezy_order_id: number | null
+          lemon_squeezy_product_id: number | null
+          lemon_squeezy_renews_at: string | null
+          lemon_squeezy_subscription_id: number | null
+          lemon_squeezy_subscription_item_id: number | null
+          lemon_squeezy_variant_id: number | null
+          tier: Database["public"]["Enums"]["premium_tier"] | null
+          updated_at: string | null
+        }
+        Insert: {
+          ai_automation_access?:
+            | Database["public"]["Enums"]["feature_access"]
+            | null
+          ai_credits?: number | null
+          ai_month?: number | null
+          approve_or_ban_credits?: number | null
+          approve_or_ban_month?: number | null
+          bulk_approve_or_ban_access?:
+            | Database["public"]["Enums"]["feature_access"]
+            | null
+          created_at?: string | null
+          id?: string
+          lemon_license_instance_id?: string | null
+          lemon_license_key?: string | null
+          lemon_squeezy_customer_id?: number | null
+          lemon_squeezy_order_id?: number | null
+          lemon_squeezy_product_id?: number | null
+          lemon_squeezy_renews_at?: string | null
+          lemon_squeezy_subscription_id?: number | null
+          lemon_squeezy_subscription_item_id?: number | null
+          lemon_squeezy_variant_id?: number | null
+          tier?: Database["public"]["Enums"]["premium_tier"] | null
+          updated_at?: string | null
+        }
+        Update: {
+          ai_automation_access?:
+            | Database["public"]["Enums"]["feature_access"]
+            | null
+          ai_credits?: number | null
+          ai_month?: number | null
+          approve_or_ban_credits?: number | null
+          approve_or_ban_month?: number | null
+          bulk_approve_or_ban_access?:
+            | Database["public"]["Enums"]["feature_access"]
+            | null
+          created_at?: string | null
+          id?: string
+          lemon_license_instance_id?: string | null
+          lemon_license_key?: string | null
+          lemon_squeezy_customer_id?: number | null
+          lemon_squeezy_order_id?: number | null
+          lemon_squeezy_product_id?: number | null
+          lemon_squeezy_renews_at?: string | null
+          lemon_squeezy_subscription_id?: number | null
+          lemon_squeezy_subscription_item_id?: number | null
+          lemon_squeezy_variant_id?: number | null
+          tier?: Database["public"]["Enums"]["premium_tier"] | null
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
+      prompt_history: {
+        Row: {
+          created_at: string | null
+          id: string
+          prompt: string
+          updated_at: string | null
+          user_id: string
+        }
+        Insert: {
+          created_at?: string | null
+          id?: string
+          prompt: string
+          updated_at?: string | null
+          user_id: string
+        }
+        Update: {
+          created_at?: string | null
+          id?: string
+          prompt?: string
+          updated_at?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "fk_user"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      rule: {
+        Row: {
+          automate: boolean | null
+          created_at: string | null
+          id: string
+          instructions: string
+          name: string
+          type: Database["public"]["Enums"]["rule_type"] | null
+          updated_at: string | null
+          user_id: string
+        }
+        Insert: {
+          automate?: boolean | null
+          created_at?: string | null
+          id?: string
+          instructions: string
+          name: string
+          type?: Database["public"]["Enums"]["rule_type"] | null
+          updated_at?: string | null
+          user_id: string
+        }
+        Update: {
+          automate?: boolean | null
+          created_at?: string | null
+          id?: string
+          instructions?: string
+          name?: string
+          type?: Database["public"]["Enums"]["rule_type"] | null
+          updated_at?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "fk_user"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       users: {
         Row: {
+          about: string | null
           access_token: string | null
-          aimodel: string | null
-          aiprovider: string | null
+          ai_model: string | null
+          ai_provider: string | null
           avatar_url: string
+          behavior_profile: Json | null
+          completed_onboarding: boolean | null
           created_at: string | null
           email: string
           id: string
+          last_login: string | null
           name: string | null
-          openaiapikey: string | null
+          onboarding_answers: Json | null
+          openai_api_key: string | null
+          premium_admin_id: string | null
+          premium_id: string | null
           refresh_token: string | null
           type: string | null
         }
         Insert: {
+          about?: string | null
           access_token?: string | null
-          aimodel?: string | null
-          aiprovider?: string | null
+          ai_model?: string | null
+          ai_provider?: string | null
           avatar_url: string
+          behavior_profile?: Json | null
+          completed_onboarding?: boolean | null
           created_at?: string | null
           email: string
           id: string
+          last_login?: string | null
           name?: string | null
-          openaiapikey?: string | null
+          onboarding_answers?: Json | null
+          openai_api_key?: string | null
+          premium_admin_id?: string | null
+          premium_id?: string | null
           refresh_token?: string | null
           type?: string | null
         }
         Update: {
+          about?: string | null
           access_token?: string | null
-          aimodel?: string | null
-          aiprovider?: string | null
+          ai_model?: string | null
+          ai_provider?: string | null
           avatar_url?: string
+          behavior_profile?: Json | null
+          completed_onboarding?: boolean | null
           created_at?: string | null
           email?: string
           id?: string
+          last_login?: string | null
           name?: string | null
-          openaiapikey?: string | null
+          onboarding_answers?: Json | null
+          openai_api_key?: string | null
+          premium_admin_id?: string | null
+          premium_id?: string | null
           refresh_token?: string | null
           type?: string | null
         }
         Relationships: [
+          {
+            foreignKeyName: "user_premium_admin_id_fkey"
+            columns: ["premium_admin_id"]
+            isOneToOne: false
+            referencedRelation: "premium"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "user_premium_id_fkey"
+            columns: ["premium_id"]
+            isOneToOne: false
+            referencedRelation: "premium"
+            referencedColumns: ["id"]
+          },
           {
             foreignKeyName: "users_id_fkey"
             columns: ["id"]
@@ -88,7 +433,20 @@ export type Database = {
       [_ in never]: never
     }
     Enums: {
-      [_ in never]: never
+      action_type: "REPLY" | "PUBLISH" | "REJECT" | "REVIEW" | "MARK_SPAM"
+      author_status: "APPROVED" | "BANNED"
+      executed_rule_status: "APPLIED" | "REJECTED" | "PENDING" | "SKIPPED"
+      feature_access: "UNLOCKED" | "UNLOCKED_WITH_API_KEY" | "LOCKED"
+      premium_tier:
+        | "BASIC_MONTHLY"
+        | "BASIC_ANNUALLY"
+        | "PRO_MONTHLY"
+        | "PRO_ANNUALLY"
+        | "BUSINESS_MONTHLY"
+        | "BUSINESS_ANNUALLY"
+        | "COPILOT_MONTHLY"
+        | "LIFETIME"
+      rule_type: "AI" | "STATIC"
     }
     CompositeTypes: {
       [_ in never]: never
