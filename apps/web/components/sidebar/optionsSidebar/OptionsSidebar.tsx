1285e06d787288c0105a375df24511c8f0957227
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-26 18:23:47 +0530

    Added toggle functionality for OptionsSidebar with responsive button controls

--- apps/web/components/sidebar/optionsSidebar/OptionsSidebar.tsx
+++ apps/web/components/sidebar/optionsSidebar/OptionsSidebar.tsx
@@ -1,23 +1,55 @@
 "use client";
 
+import { useState } from "react";
 import { usePathname } from "@/utils/navigation";
 import { ScrollArea } from "@openstudio/ui/components/ui/scroll-area";
 import { Settings } from "@/components/sidebar/optionsSidebar/settingsOptions/Settings";
+import { CommentsSidebar } from "./commentsOptions/Comments";
 
 interface OptionsSidebarProps {}
 
 export const OptionsSidebar = ({}: OptionsSidebarProps) => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(true);
 
-  if (pathname === "/dashboard" || pathname === "/dashboard/comments") {
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  if (pathname.includes("/dashboard/premium") || pathname === "/dashboard") {
     return null;
   }
 
   return (
-    <div className="h-full p-4 sm:py-6 border-r w-52 sm:w-64 flex flex-col justify-between gap-2">
-      <ScrollArea className="h-full">
-        {pathname.includes("/dashboard/settings") && <Settings />}
-      </ScrollArea>
-    </div>
+    <>
+      <div
+        className={`h-full ${
+          isOpen ? "border-r w-52 sm:w-64 p-4 sm:py-6" : "w-0 border-r-0"
+        } flex flex-col justify-between gap-2 transition-width duration-300`}
+      >
+        {isOpen && (
+          <ScrollArea className="h-full">
+            {pathname.includes("/dashboard/settings") && <Settings />}
+            {pathname.startsWith("/dashboard/comments") && <CommentsSidebar />}
+          </ScrollArea>
+        )}
+      </div>
+      {!isOpen && (
+        <button
+          onClick={toggleSidebar}
+          className="fixed left-14 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-2 rounded-r-md"
+        >
+          &gt;
+        </button>
+      )}
+      {isOpen && (
+        <button
+          onClick={toggleSidebar}
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-2 rounded-l-md"
+        >
+          &lt;
+        </button>
+      )}
+    </>
   );
 };
