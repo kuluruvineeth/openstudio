72581f109d8f6a753e694a30bab72245a55e8026
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-08-26 19:55:29 +0530

    created app types based on db types

--- apps/web/types/app.ts
+++ apps/web/types/app.ts
@@ -1,15 +1,44 @@
 export type User = {
+  about: string | null;
   access_token: string | null;
-  aimodel: string | null;
-  aiprovider: string | null;
+  ai_model: string | null;
+  ai_provider: string | null;
   avatar_url: string;
+  behavior_profile: any | null;
+  completed_onboarding: boolean | null;
   created_at: string | null;
   email: string;
   id: string;
+  last_login: string | null;
   name: string | null;
-  openaiapikey: string | null;
+  onboarding_answers: any | null;
+  openai_api_key: string | null;
+  premium_admin_id: string | null;
+  premium_id: string | null;
   refresh_token: string | null;
   type: string | null;
+};
+
+export type Premium = {
+  ai_automation_access: FeatureAccessType | null;
+  ai_credits: number | null;
+  ai_month: number | null;
+  approve_or_ban_credits: number | null;
+  approve_or_ban_month: number | null;
+  bulk_approve_or_ban_access: FeatureAccessType | null;
+  created_at: string | null;
+  id: string;
+  lemon_license_instance_id: string | null;
+  lemon_license_key: string | null;
+  lemon_squeezy_customer_id: number | null;
+  lemon_squeezy_order_id: number | null;
+  lemon_squeezy_product_id: number | null;
+  lemon_squeezy_renews_at: string | null;
+  lemon_squeezy_subscription_id: number | null;
+  lemon_squeezy_subscription_item_id: number | null;
+  lemon_squeezy_variant_id: number | null;
+  tier: PremiumTierType | null;
+  updated_at: string | null;
 };
 
 export interface DateRange {
@@ -17,8 +46,48 @@ export interface DateRange {
   to: Date | undefined;
 }
 
+export const ActionType = {
+  REPLY: "REPLY",
+  PUBLISH: "PUBLISH",
+  REJECT: "REJECT",
+  REVIEW: "REVIEW",
+  MARK_SPAM: "MARK_SPAM",
+} as const;
+export type ActionTypeType = (typeof ActionType)[keyof typeof ActionType];
+
 export const AuthorStatus = {
   APPROVED: "APPROVED",
   BANNED: "BANNED",
 } as const;
 export type AuthorStatusType = (typeof AuthorStatus)[keyof typeof AuthorStatus];
+
+export const ExecutedRuleStatus = {
+  APPLIED: "APPLIED",
+  REJECTED: "REJECTED",
+  PENDING: "PENDING",
+  SKIPPED: "SKIPPED",
+} as const;
+export type ExecutedRuleStatusType =
+  (typeof ExecutedRuleStatus)[keyof typeof ExecutedRuleStatus];
+
+export const FeatureAccess = {
+  UNLOCKED: "UNLOCKED",
+  UNLOCKED_WITH_API_KEY: "UNLOCKED_WITH_API_KEY",
+  LOCKED: "LOCKED",
+} as const;
+export type FeatureAccessType =
+  (typeof FeatureAccess)[keyof typeof FeatureAccess];
+export const PremiumTier = {
+  BASIC_MONTHLY: "BASIC_MONTHLY",
+  BASIC_ANNUALLY: "BASIC_ANNUALLY",
+  PRO_MONTHLY: "PRO_MONTHLY",
+  PRO_ANNUALLY: "PRO_ANNUALLY",
+  BUSINESS_MONTHLY: "BUSINESS_MONTHLY",
+  BUSINESS_ANNUALLY: "BUSINESS_ANNUALLY",
+  COPILOT_MONTHLY: "COPILOT_MONTHLY",
+  LIFETIME: "LIFETIME",
+} as const;
+
+export type PremiumTierType = (typeof PremiumTier)[keyof typeof PremiumTier];
+
+export type RuleType = "AI" | "STATIC";
