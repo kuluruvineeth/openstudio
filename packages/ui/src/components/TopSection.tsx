61d62ed8a651e78862f1c1b5f3e24c820252cb67
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-06 09:44:57 +0530

    Introduced TopSectionWithRightSection component with support for right-aligned custom component

--- packages/ui/src/components/TopSection.tsx
+++ packages/ui/src/components/TopSection.tsx
@@ -20,3 +20,28 @@ export function TopSection(props: {
     </div>
   );
 }
+
+export function TopSectionWithRightSection(props: {
+  title: string;
+  description?: string;
+  descriptionComponent?: React.ReactNode;
+  rightComponent: React.ReactNode;
+}) {
+  return (
+    <div className="content-container flex items-center justify-between border-b border-gray-200 bg-white py-6 shadow-sm">
+      <div>
+        <PageHeading>{props.title}</PageHeading>
+        <div className="mt-2">
+          {props.descriptionComponent ? (
+            props.descriptionComponent
+          ) : (
+            <SectionDescription className="max-w-prose">
+              {props.description}
+            </SectionDescription>
+          )}
+        </div>
+      </div>
+      <div>{props.rightComponent}</div>
+    </div>
+  );
+}
