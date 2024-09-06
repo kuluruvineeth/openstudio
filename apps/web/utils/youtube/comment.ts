16fbef8431cfeaeff5b78027360b3f50c4e0db5d
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-06 10:06:31 +0530

    Implemented `replyToComment` function to post a reply to a YouTube comment.

--- apps/web/utils/youtube/comment.ts
+++ apps/web/utils/youtube/comment.ts
@@ -1,4 +1,23 @@
 import { type youtube_v3 } from "googleapis";
+
+export async function replyToComment(options: {
+  youtube: youtube_v3.Youtube;
+  commentId: string;
+  content: string;
+}) {
+  const { youtube, commentId, content } = options;
+
+  await youtube.comments.insert({
+    part: ["snippet"],
+    requestBody: {
+      snippet: {
+        parentId: commentId,
+        textOriginal: content,
+      },
+    },
+  });
+}
+
 export async function getComment(
   commentId: string,
   youtube: youtube_v3.Youtube,
