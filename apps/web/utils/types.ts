ddef0c4918e747841ee7a3ada57189b238cc43ba
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-06 10:47:14 +0530

    Implemented `isDefined` type guard to filter out `undefined` and `null` values.

--- apps/web/utils/types.ts
+++ apps/web/utils/types.ts
@@ -1,2 +1,7 @@
 // https://stackoverflow.com/a/53276873/2602771
 export type PartialRecord<K extends keyof any, T> = Partial<Record<K, T>>;
+
+// type guard for filters that removed undefined and null values
+export function isDefined<T>(value: T | undefined | null): value is T {
+  return value !== undefined && value !== null;
+}
