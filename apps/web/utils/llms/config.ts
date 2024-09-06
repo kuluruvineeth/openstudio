04745e3ea14f15cd9c1339beddbcb9d7dccd6b33
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-06 10:18:17 +0530

    Added `Model` constants to include `"gpt-4o"`, `"gpt-4o-mini"`, `"anthropic.claude-3-5-sonnet-20240620-v1:0"`, and `"claude-3-5-sonnet-20240620"`.

--- apps/web/utils/llms/config.ts
+++ apps/web/utils/llms/config.ts
@@ -2,3 +2,11 @@ export const Provider = {
   OPEN_AI: "openai",
   ANTHROPIC: "anthropic",
 };
+
+export const Model = {
+  GPT_4O: "gpt-4o",
+  GPT_4O_MINI: "gpt-4o-mini",
+  CLAUDE_3_5_SONNET_BEDROCK: "anthropic.claude-3-5-sonnet-20240620-v1:0",
+  CLAUDE_3_5_SONNET_ANTHROPIC: "claude-3-5-sonnet-20240620",
+};
+
