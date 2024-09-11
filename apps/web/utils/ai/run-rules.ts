cd038e8ebc392f529d0463aef100ff938ad389eb
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-11 07:23:09 +0530

    Implemented runRulesOnComment function to apply AI rules, handle execution, and log skipped rules

--- apps/web/utils/ai/run-rules.ts
+++ apps/web/utils/ai/run-rules.ts
@@ -5,6 +5,45 @@ import { ExecutedRuleStatus, Rule, User } from "@/types/app";
 import { UserAIFields } from "../llms/types";
 import { chooseRuleAndExecute } from "./choose-and-execute";
 import { supabaseServerClient } from "@/supabase/supabaseServer";
+
+export async function runRulesOnComment({
+  youtube,
+  comment,
+  rules,
+  user,
+}: {
+  youtube: youtube_v3.Youtube;
+  comment: CommentForAction;
+  rules: RulesResponse | any;
+  user: Pick<User, "id" | "email" | "prompt"> & UserAIFields;
+}): Promise<{
+  handled: boolean;
+}> {
+  // ai rules
+  const aiRules = rules.filter((r) => r.type === Rule.AI);
+
+  const aiResponse = await chooseRuleAndExecute({
+    comment,
+    rules: aiRules,
+    youtube,
+    user,
+    isTest: false,
+  });
+
+  if (aiResponse.handled) return { handled: true };
+
+  console.log(`No rules matched. ${user.email} ${comment.commentId}`);
+
+  // no rules matched
+  await saveSkippedExecutedRule({
+    userId: user.id,
+    commentId: comment.commentId,
+    reason: aiResponse?.reason,
+  });
+
+  return { handled: false };
+}
+
 async function saveSkippedExecutedRule({
   userId,
   commentId,
