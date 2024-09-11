e6da2453f9d3a0dcc3a95249a93b8bccc0fe1111
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-11 07:10:43 +0530

    Added getArgsAiResponse to generate AI arguments for a selected rule, handle comments, and call chat completion tools

--- apps/web/utils/ai/ai-choose-args.ts
+++ apps/web/utils/ai/ai-choose-args.ts
@@ -1,7 +1,12 @@
 import { Action, ActionTypeType, User } from "@/types/app";
 import { z } from "zod";
 import { isDefined } from "../types";
+import { CommentForLLM, stringifyComment } from "./stringify-comment";
+import { UserAIFields } from "../llms/types";
+import { RulesResponse } from "@/app/api/user/rules/route";
+import { chatCompletionTools } from "../llms";
 import { ActionItem } from "./actions";
+
 type AIGeneratedArgs = Record<
   ActionTypeType,
   Record<keyof Omit<ActionItem, "type">, string>
@@ -56,6 +61,72 @@ function getToolParamtersForRule(actions: Action[]) {
 
   return parameters;
 }
+
+export async function getArgsAiResponse({
+  comment,
+  user,
+  selectedRule,
+}: {
+  comment: CommentForLLM;
+  user: Pick<User, "email" | "prompt"> & UserAIFields;
+  selectedRule: RulesResponse[0];
+}) {
+  console.log(
+    `Generating args for rule ${selectedRule.name} (${selectedRule.id})`,
+  );
+
+  const paramters = getToolParamtersForRule(selectedRule.action as any);
+
+  if (!Object.keys(paramters).length) {
+    console.log(
+      `Skipping. No parameters for rule ${selectedRule.name} (${selectedRule.id})`,
+    );
+    return;
+  }
+
+  const system = `You are an AI assistant that helps people manage their youtube comments.
+  Never put placeholders in your comment responses.
+  Do not mention you are an AI assistant when responding to people.
+  ${
+    user.prompt
+      ? `\nSome additional information the user has provided about themselves:\n\n${user.prompt}`
+      : ""
+  }`;
+
+  const prompt = `A Comment was received for processing and the following rule was selected to process it:
+  ###
+  ${selectedRule.instructions}
+  ###
+  
+  Handle the comment.
+  
+  The comment:
+  ${stringifyComment(comment, 3000)}`;
+
+  console.log("Calling chat completion tools");
+
+  const aiResponse = await chatCompletionTools({
+    userAi: user,
+    prompt,
+    system,
+    tools: {
+      apply_rule: {
+        description: `Apply the rule with given arguments`,
+        parameters: z.object(paramters),
+      },
+    },
+    label: "Args for rule",
+    userEmail: user.email || "",
+  });
+
+  const toolCall = aiResponse.toolCalls[0];
+
+  if (!toolCall) return;
+  if (!toolCall.toolName) return;
+
+  return toolCall.args;
+}
+
 export function getActionItemsFromAiArgsResponse(
   response: AIGeneratedArgs | undefined,
   ruleActions: Action[],
