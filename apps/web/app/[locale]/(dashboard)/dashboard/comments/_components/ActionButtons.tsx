ea4a8f239fb479ba973019b9a2cecd25d973868c
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-12 07:26:52 +0530

    Added "Run AI Rules" and "Delete" buttons with respective loading states

--- apps/web/app/[locale]/(dashboard)/dashboard/comments/_components/ActionButtons.tsx
+++ apps/web/app/[locale]/(dashboard)/dashboard/comments/_components/ActionButtons.tsx
@@ -1,16 +1,36 @@
+// import { onDeleteComment } from "@/utils/actions/client";
 import { ButtonGroup } from "@openstudio/ui/components/ButtonGroup";
 import { LoadingMiniSpinner } from "@openstudio/ui/components/Loading";
-import { OrbitIcon } from "lucide-react";
-import { useMemo } from "react";
+import { OrbitIcon, SparklesIcon, Trash2Icon } from "lucide-react";
+import { useCallback, useMemo, useState } from "react";
 
 export function ActionButtons(props: {
   commentId: string;
+  isPlanning: boolean;
   shadow?: boolean;
   isCategorizing: boolean;
+  onPlanAiAction: () => void;
   onAiCategorize: () => void;
   refetch: () => void;
 }) {
-  const { commentId, shadow, isCategorizing, onAiCategorize, refetch } = props;
+  const {
+    commentId,
+    shadow,
+    isPlanning,
+    onPlanAiAction,
+    isCategorizing,
+    onAiCategorize,
+    refetch,
+  } = props;
+
+  const [isdeleting, setIsDeleting] = useState(false);
+
+  const onDelete = useCallback(async () => {
+    setIsDeleting(true);
+    // await onDeleteComment(commentId);
+    refetch();
+    setIsDeleting(false);
+  }, [commentId, refetch]);
 
   const buttons = useMemo(
     () => [
@@ -23,8 +43,33 @@ export function ActionButtons(props: {
           <OrbitIcon className="h-4 w-4 text-gray-700" aria-hidden="true" />
         ),
       },
+      {
+        tooltip: "Run AI Rules",
+        onClick: onPlanAiAction,
+        icon: isPlanning ? (
+          <LoadingMiniSpinner />
+        ) : (
+          <SparklesIcon className="h-4 w-4 text-gray-700" />
+        ),
+      },
+      {
+        tooltip: "Delete",
+        onClick: onDelete,
+        icon: isdeleting ? (
+          <LoadingMiniSpinner />
+        ) : (
+          <Trash2Icon className="h-4 w-4 text-gray-700" aria-hidden="true" />
+        ),
+      },
     ],
-    [isCategorizing, onAiCategorize],
+    [
+      isCategorizing,
+      onAiCategorize,
+      onDelete,
+      isdeleting,
+      onPlanAiAction,
+      isPlanning,
+    ],
   );
 
   return <ButtonGroup buttons={buttons} shadow={shadow} />;
