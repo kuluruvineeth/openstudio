f29c28540036f87caaf0b36f1de786a487d9c3b1
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-12 06:52:10 +0530

    Added PromptSectionForm with form validation and submit handling using react-hook-form

---
+++ apps/web/app/[locale]/(dashboard)/dashboard/settings/_components/PromptSectionForm.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Button } from "@openstudio/ui/components/Button";
+import {
+  FormSection,
+  FormSectionLeft,
+  FormSectionRight,
+  SubmitButtonWrapper,
+} from "@openstudio/ui/components/Form";
+import { Input } from "@openstudio/ui/components/Input";
+import { useForm } from "react-hook-form";
+
+export const PromptSectionForm = (props: { prompt?: string }) => {
+  const {
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<any>({
+    defaultValues: { prompt: props.prompt },
+  });
+
+  return (
+    <form action={async () => {}}>
+      <FormSection>
+        <FormSectionLeft
+          title="Default Prompt Settings"
+          description="Provide extra information to LLM Models for better one short learning to help it write better replies for you."
+        />
+        <div className="md:col-span-2">
+          <FormSectionRight>
+            <div className="sm:col-span-full">
+              <Input
+                type="text"
+                as="textarea"
+                rows={8}
+                name="prompt"
+                label="Default Prompt"
+                registerProps={(register("prompt"), { required: true })}
+                error={errors.prompt as any}
+                placeholder={`Some rules to follow:
+* Be friendly, concise, and professional, but not overly formal.
+* Keep responses short and to the point.`}
+              />
+            </div>
+          </FormSectionRight>
+          <SubmitButtonWrapper>
+            <Button
+              color={"green"}
+              type="submit"
+              size="lg"
+              loading={isSubmitting}
+            >
+              Save
+            </Button>
+          </SubmitButtonWrapper>
+        </div>
+      </FormSection>
+    </form>
+  );
+};
