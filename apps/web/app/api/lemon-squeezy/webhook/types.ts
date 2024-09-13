c8f3bb6c0cde694b57865838d81d9cc468b716ac
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-13 09:04:32 +0530

    Added EventName type with subscription and order event options

---
+++ apps/web/app/api/lemon-squeezy/webhook/types.ts
@@ -0,0 +1,13 @@
+export type EventName =
+  | "order_created"
+  | "order_refunded"
+  | "subscription_created"
+  | "subscription_updated"
+  | "subscription_cancelled"
+  | "subscription_resumed"
+  | "subscription_expired"
+  | "subscription_paused"
+  | "subscription_unpaused"
+  | "subscription_payment_failed"
+  | "subscription_payment_success"
+  | "subscription_payment_recovered";
