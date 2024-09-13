d0c73e96fda53c865b13c62ff1961a787c2a9b2a
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-13 09:26:21 +0530

    Implemented `getUserPlan` to determine premium tier based on renewal date

--- apps/web/actions/premium/index.ts
+++ apps/web/actions/premium/index.ts
@@ -9,3 +9,24 @@ import {
 export const isPremium = (lemonSqueezyRenewsAt: Date | null): boolean => {
   return !!lemonSqueezyRenewsAt && new Date(lemonSqueezyRenewsAt) > new Date();
 };
+
+const getUserPlan = (
+  lemonSqueezyRenewsAt?: string | null,
+): PremiumTierType | null => {
+  if (!lemonSqueezyRenewsAt) return null;
+
+  const renewsAt = new Date(lemonSqueezyRenewsAt);
+
+  // if renewsAt is 5 years in the future then it's a lifetime plan
+  if (renewsAt.getFullYear() - new Date().getFullYear() >= 5) {
+    return PremiumTier.LIFETIME;
+  }
+
+  // if renewsAt is more than 6 months in the future then it's annual plan
+  if (renewsAt > new Date(new Date().setMonth(new Date().getMonth() + 6))) {
+    return PremiumTier.BASIC_ANNUALLY;
+  }
+
+  // if renewsAt is less than 6 months in the future then it's a monthly plan
+  return PremiumTier.BASIC_MONTHLY;
+};
