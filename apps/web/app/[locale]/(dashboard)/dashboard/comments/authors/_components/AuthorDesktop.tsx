606fbd958ee7058053c4eedcbcaff452c1397574
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-15 10:52:57 +0530

    Implemented AuthorRowDesktop component for desktop view with ActionCell integration and row styling

---
+++ apps/web/app/[locale]/(dashboard)/dashboard/comments/authors/_components/AuthorDesktop.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+} from "@tremor/react";
+import React from "react";
+import { HeaderButton } from "./HeaderButton";
+import { RowProps } from "./types";
+import { ActionCell } from "./ActionCell";
+export function AuthorRowDesktop(props: RowProps) {
+  const { item, refetchPremium } = props;
+
+  return (
+    <TableRow
+      key={item.name}
+      className={props.selected ? "bg-red-50" : undefined}
+      aria-selected={props.selected || undefined}
+      data-selected={props.selected || undefined}
+      onMouseEnter={props.onSelectRow}
+      onDoubleClick={props.onDoubleClick}
+    >
+      <TableCell className="max-w-[250px] truncate pl-6 min-[1550px]:max-w-[300px] min-[1650px]:max-w-none">
+        {item.name}
+      </TableCell>
+      <TableCell>{item.value}</TableCell>
+      <TableCell className="flex justify-end gap-2 p-2">
+        <ActionCell
+          item={item}
+          hasApproveorBanAccess={props.hasApproveorBanAccess}
+          mutate={props.mutate}
+          refetchPremium={props.refetchPremium}
+          setOpenedAuthor={props.setOpenedAuthor}
+          selected={props.selected}
+          openPremiumModal={props.openPremiumModal}
+          authorDisplayName={props.authorDisplayName}
+        />
+      </TableCell>
+    </TableRow>
+  );
+}
