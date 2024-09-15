ef2a8cb162aa8bf8ede31913dde780fa38360a33
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-15 09:53:03 +0530

    Added `attachUserInfo` function to append user info to URL

--- apps/web/app/[locale]/(dashboard)/dashboard/premium/_components/Pricing.tsx
+++ apps/web/app/[locale]/(dashboard)/dashboard/premium/_components/Pricing.tsx
@@ -4,3 +4,11 @@ function formatPrice(price: number) {
   if (price - Math.floor(price) > 0) return price.toFixed(2);
   return price;
 }
+function attachUserInfo(
+  url: string,
+  user: { id: string; email: string; name?: string | null },
+) {
+  if (!user) return url;
+
+  return `${url}?checkout[custom][user_id]=${user.id}&checkout[email]=${user.email}&checkout[name]=${user.name}`;
+}
