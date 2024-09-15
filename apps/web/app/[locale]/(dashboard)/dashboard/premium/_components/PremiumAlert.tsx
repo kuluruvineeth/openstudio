19e1cf009bf769592da1e5f1602f67b44225498c
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-15 10:07:31 +0530

    Added `PremiumAlert` component to prompt users for premium upgrade and API key setup

--- apps/web/app/[locale]/(dashboard)/dashboard/premium/_components/PremiumAlert.tsx
+++ apps/web/app/[locale]/(dashboard)/dashboard/premium/_components/PremiumAlert.tsx
@@ -9,6 +9,11 @@ import { UserResponse } from "@/app/api/user/me/route";
 import { PremiumTier } from "@/types/app";
 import { api } from "@/utils/api";
 import { useQuery } from "@tanstack/react-query";
+import { usePremiumModal } from "./PremiumModal";
+import { AlertWithButton } from "@openstudio/ui/components/Alert";
+import Link from "next/link";
+import { CrownIcon } from "lucide-react";
+import { Button } from "@openstudio/ui/components/Button";
 import React from "react";
 export function usePremium() {
   const { data, isLoading, error, refetch } = useQuery<UserResponse>({
@@ -46,4 +51,46 @@ export function usePremium() {
     hasAiAccess: hasAiAccess(premium?.ai_automation_access),
     isProPlanWithoutApiKey,
   };
+}
+
+function PremiumAlert({
+  plan = "Open Studio Business",
+  showSetApiKey,
+}: {
+  plan?: "Open Studio Business" | "Open Studio Pro";
+  showSetApiKey: boolean;
+}) {
+  const { PremiumModal, openModal } = usePremiumModal();
+
+  return (
+    <>
+      <AlertWithButton
+        title="Premium"
+        description={
+          <>
+            This is a premium feature. Upgrade to {plan}
+            {showSetApiKey ? (
+              <>
+                {" "}
+                or set an OpenAI API key on the{" "}
+                <Link
+                  href={"/settings"}
+                  className="font-semibold hover:text-gray-700"
+                >
+                  settings
+                </Link>{" "}
+                page.
+              </>
+            ) : (
+              <></>
+            )}
+          </>
+        }
+        icon={<CrownIcon className="h-4 w-4" />}
+        button={<Button onClick={openModal}>Upgrade</Button>}
+        variant="red"
+      />
+      <PremiumModal />
+    </>
+  );
 }
