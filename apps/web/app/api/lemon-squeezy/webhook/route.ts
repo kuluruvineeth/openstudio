f2bc901f2969aa5535357cecb81681009efb89d1
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-15 10:44:19 +0530

    Handled subscription cancellation by updating premium status and logging event

--- apps/web/app/api/lemon-squeezy/webhook/route.ts
+++ apps/web/app/api/lemon-squeezy/webhook/route.ts
@@ -1,5 +1,48 @@
 import { env } from "@/env.mjs";
 import { PremiumTier, PremiumTierType } from "@/types/app";
+import { Payload } from "./types";
+import {
+  cancelPremium,
+} from "@/actions/premium/server";
+import { posthogCaptureEvent } from "@/utils/posthog";
+import { NextResponse } from "next/server";
+import { supabaseServerClient } from "@/supabase/supabaseServer";
+
+async function subscriptionCancelled({
+  payload,
+  premium_id,
+  ends_at,
+}: {
+  payload: Payload;
+  premium_id: string;
+  ends_at: NonNullable<Payload["data"]["attributes"]["ends_at"]>;
+}) {
+  const updatedPremium = await cancelPremium({
+    premium_id,
+    lemon_squeezy_renews_at: new Date(ends_at).toISOString(),
+  });
+
+  const supabase = await supabaseServerClient();
+
+  const { data, error } = await supabase
+    .from("users")
+    .select("email")
+    .eq("id", payload.meta.custom_data?.user_id)
+    .single();
+
+  if (data?.email) {
+    await Promise.allSettled([
+      await posthogCaptureEvent(data.email, "Cancelled premium subscription", {
+        ...payload.data.attributes,
+        $set: { premium: false, premiumCancelled: true },
+      }),
+      //TODO: send cancellation email
+    ]);
+  }
+
+  return NextResponse.json({ ok: true });
+}
+
 function getSubscriptionTier({
   variantId,
 }: {
