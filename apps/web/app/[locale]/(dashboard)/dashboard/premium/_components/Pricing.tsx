13679278d2009932105bbe6a10870d1754c6ca36
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-15 09:54:56 +0530

    Added `buildLemonUrl` function to append affiliate code to URL

--- apps/web/app/[locale]/(dashboard)/dashboard/premium/_components/Pricing.tsx
+++ apps/web/app/[locale]/(dashboard)/dashboard/premium/_components/Pricing.tsx
@@ -4,6 +4,7 @@ function formatPrice(price: number) {
   if (price - Math.floor(price) > 0) return price.toFixed(2);
   return price;
 }
+
 function attachUserInfo(
   url: string,
   user: { id: string; email: string; name?: string | null },
@@ -11,4 +12,10 @@ function attachUserInfo(
   if (!user) return url;
 
   return `${url}?checkout[custom][user_id]=${user.id}&checkout[email]=${user.email}&checkout[name]=${user.name}`;
+}
+
+function buildLemonUrl(url: string, affiliateCode?: string | null) {
+  if (!affiliateCode) return url;
+  const newUrl = `${url}?aff_ref=${affiliateCode}`;
+  return newUrl;
 }
