fe13efdf42ba5e94a4377aa83ca82bf732bdfb8f
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-15 10:48:39 +0530

    Handled various Lemon Squeezy webhook events with specific actions for subscription creation, updates, cancellations, and order processing

--- apps/web/app/api/lemon-squeezy/webhook/route.ts
+++ apps/web/app/api/lemon-squeezy/webhook/route.ts
@@ -9,7 +9,68 @@ import {
 } from "@/actions/premium/server";
 import { posthogCaptureEvent } from "@/utils/posthog";
 import { NextResponse } from "next/server";
+import { withError } from "@/utils/middleware";
 import { supabaseServerClient } from "@/supabase/supabaseServer";
+
+export const POST = withError(async (request: Request) => {
+  const payload = await getPayload(request);
+  const userId = payload.meta.custom_data?.user_id;
+
+  console.log("===Lemon event type:", payload.meta.event_name);
+
+  // ignored events
+  if (["subscription_payment_success"].includes(payload.meta.event_name)) {
+    return NextResponse.json({ ok: true });
+  }
+
+  // monthly/annual subscription
+  if (payload.meta.event_name === "subscription_created") {
+    if (!userId) throw new Error("No userId provided");
+    return await subscriptionCreated({ payload, userId });
+  }
+
+  const variant = payload.data.attributes.first_order_item?.variant_id;
+  const isLifetimePlan = variant === env.NEXT_PUBLIC_LIFETIME_VARIANT_ID;
+
+  // lifetime plan
+  if (payload.meta.event_name === "order_created" && isLifetimePlan) {
+    if (!userId) throw new Error("No userId provided");
+    return await lifetimeOrder({ payload, userId });
+  }
+
+  const lemon_squeezy_customer_id = payload.data.attributes.customer_id;
+
+  const supabase = await supabaseServerClient();
+
+  const { data, error } = await supabase
+    .from("premium")
+    .select("id")
+    .eq("lemon_squeezy_customer_id", lemon_squeezy_customer_id)
+    .single();
+
+  if (!data?.id) {
+    throw new Error(
+      `No user found for lemon_squeezy_customer_id ${lemon_squeezy_customer_id}`,
+    );
+  }
+
+  // renewal
+  if (payload.meta.event_name === "subscription_updated") {
+    return await subscriptionUpdated({ payload, premium_id: data.id });
+  }
+
+  // cancellation
+  if (payload.data.attributes.ends_at) {
+    return await subscriptionCancelled({
+      payload,
+      premium_id: data.id,
+      ends_at: payload.data.attributes.ends_at,
+    });
+  }
+
+  return NextResponse.json({ ok: true });
+});
+
 // https://docs.lemonsqueezy.com/help/webhooks#signing-requests
 // https://gist.github.com/amosbastian/e403e1d8ccf4f7153f7840dd11a85a69
 async function getPayload(request: Request): Promise<Payload> {
