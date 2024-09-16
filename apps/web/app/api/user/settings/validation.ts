891eef15bb277d6a11fb22a4095702bd27e5588b
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-16 08:34:25 +0530

    Added validation to `saveSettingsBody` to ensure AI model is preset if API key is not provided

---
+++ apps/web/app/api/user/settings/validation.ts
@@ -0,0 +1,19 @@
+import { modelOptions } from "@/utils/llms/config";
+import { z } from "zod";
+
+export const saveSettingsBody = z
+  .object({
+    aiProvider: z.string(),
+    aiModel: z.string(),
+    aiApiKey: z.string().optional(),
+  })
+  .superRefine((val) => {
+    // if ai api key is not set, model must be a preset model
+    if (!val.aiApiKey) {
+      const validModels = modelOptions[val.aiProvider]?.map((m) => m.value);
+      z.enum(validModels as [string, ...string[]]).parse(val.aiModel);
+    }
+    return true;
+  });
+
+export type SaveSettingsBody = z.infer<typeof saveSettingsBody>;
