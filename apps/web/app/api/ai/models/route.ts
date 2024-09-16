d58170869f8a389d7b147203392e1c2973c83589
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-16 08:36:52 +0530

    Refactored GET handler to fetch OpenAI models using user API key from Supabase

--- apps/web/app/api/ai/models/route.ts
+++ apps/web/app/api/ai/models/route.ts
@@ -1,8 +1,11 @@
-import { env } from "@/env.mjs";
+import { supabaseServerClient } from "@/supabase/supabaseServer";
 import { auth } from "@/utils/auth";
+import { Provider } from "@/utils/llms/config";
 import { withError } from "@/utils/middleware";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
+
+export type OpenAiModelsResponse = Awaited<ReturnType<typeof getOpenAIModels>>;
 
 async function getOpenAIModels({ apiKey }: { apiKey: string }) {
   const openai = new OpenAI({ apiKey });
@@ -18,9 +21,22 @@ export const GET = withError(async () => {
   if (!session?.ownerEmail) {
     return NextResponse.json({ error: "Not authenticated" });
   }
+  const supabase = await supabaseServerClient();
+  const { data: user, error } = await supabase
+    .from("users")
+    .select("ai_api_key, ai_provider")
+    .eq("email", session.ownerEmail)
+    .single();
 
-  //TODO: Should use user openai api key
-  const result = await getOpenAIModels({ apiKey: env.OPENAI_API_KEY });
+  if (error) {
+    console.error("Error fetching user data:", error.message);
+    return NextResponse.json({ error: "User not found" });
+  }
+
+  if (!user.ai_api_key || user.ai_provider !== Provider.OPEN_AI)
+    return NextResponse.json([]);
+
+  const result = await getOpenAIModels({ apiKey: user.ai_api_key });
 
   return NextResponse.json(result);
 });
