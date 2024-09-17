136e3dd34a1b1cd32692e86a6b7cfe4abc73600d
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-17 07:13:10 +0530

    Implemented account deletion function with error handling and multiple service integrations.

--- apps/web/utils/actions/user.ts
+++ apps/web/utils/actions/user.ts
@@ -4,6 +4,12 @@ import { z } from "zod";
 import { ServerActionResponse } from "../error";
 import { auth } from "../auth";
 import { supabaseServerClient } from "@/supabase/supabaseServer";
+import { deleteUserStats } from "../redis/stats";
+import {
+  deleteTinybirdAiCalls,
+  deleteTinybirdComments,
+} from "@openstudio/tinybird";
+import { deletePosthogUser } from "../posthog";
 
 const savePromptBody = z.object({ prompt: z.string() });
 export type SavePromptBody = z.infer<typeof savePromptBody>;
@@ -28,4 +34,35 @@ export async function savePromptAction(
   }
 
   return { success: true, data };
+}
+
+export async function deleteAccountAction(): Promise<ServerActionResponse> {
+  const session = await auth();
+  if (!session?.ownerEmail) return { error: "Not logged in" };
+
+  //TODO: to add resend and loops
+  try {
+    await Promise.allSettled([
+      deleteUserStats({ email: session.ownerEmail }),
+      deleteTinybirdComments({ ownerEmail: session.ownerEmail }),
+      deleteTinybirdAiCalls({ userId: session.ownerEmail }),
+      deletePosthogUser({ email: session.ownerEmail }),
+    ]);
+  } catch (error) {
+    console.error("Error while deleting account: ", error);
+  }
+
+  const supabase = await supabaseServerClient();
+
+  const { data, error } = await supabase
+    .from("users")
+    .delete()
+    .eq("email", session.ownerEmail);
+
+  if (error) {
+    console.error("Error deleting the user from supabase: ", error);
+    return { success: false };
+  } else {
+    return { success: true };
+  }
 }
