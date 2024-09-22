c9f6b35a48c56e728d18482cab52b67d59d68b1b
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-22 19:34:41 +0530

    Added main container and GridPattern to MarketingLayout.

--- apps/web/app/[locale]/(marketing)/layout.tsx
+++ apps/web/app/[locale]/(marketing)/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Banner from "./_components/Banner";
 import { SiteHeader } from "./_components/SiteHeader";
+import GridPattern from "@openstudio/ui/components/GridPattern";
+import { cn } from "@openstudio/ui/lib/utils";
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -13,6 +15,16 @@ export default async function MarketingLayout({
     <>
       <Banner />
       <SiteHeader />
+      <main className="flex-1 container">{children}</main>
+      <GridPattern
+        width={40}
+        height={40}
+        x={-1}
+        y={-1}
+        className={cn(
+          "-z-10 stroke-gray-300/30 [mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)]",
+        )}
+      />
     </>
   );
 }
