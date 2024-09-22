631a1e3005381ec28b8aaef31cfd43cadc382f77
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-22 19:37:34 +0530

    Added MainNav, GitHub icon, CountingNumbers, and StarIcon to SiteHeader.

--- apps/web/app/[locale]/(marketing)/_components/SiteHeader.tsx
+++ apps/web/app/[locale]/(marketing)/_components/SiteHeader.tsx
@@ -1,6 +1,10 @@
 import { buttonVariants } from "@openstudio/ui/components/ui/button";
 import { cn } from "@openstudio/ui/lib/utils";
 import Link from "next/link";
+import { Icons } from "@openstudio/ui/components/Icons";
+import CountingNumbers from "@openstudio/ui/components/CountingNumbers";
+import { StarIcon } from "lucide-react";
+import { MainNav } from "./MainNav";
 
 export async function SiteHeader() {
   const { stargazers_count: stars } = await fetch(
@@ -23,18 +27,27 @@ export async function SiteHeader() {
   return (
     <header className="supports-backdrop-blur:bg-background/80 sticky top-0 z-40 w-full backdrop-blur bg-background border-b">
       <div className="container flex h-16 items-center">
-        <div>
-          <div>
+        <MainNav />
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-between">
+          <div className="w-full flex-1 md:w-auto md:flex-none">
             <Link
               className={cn(
-                buttonVariants(),
+                buttonVariants({ variant: "black" }),
                 "gap-2 whitespace-pre hidden md:flex",
                 "group relative justify-center gap-2 w-full transition-all duration-300 ease-out hover:ring-2 hover:ring-primary hover:ring-offset-2 rounded-sm",
               )}
               href={""}
             >
+              <span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40" />
+              <Icons.gitHub className="h-4 w-4" />
               Star on Github
-              <div>{stars}</div>
+              <div className="hidden md:flex items-center gap-1 text-sm text-gray-500">
+                <StarIcon className="h-4 w-4 group-hover:text-yellow-300 transition-all duration-300" />
+                <CountingNumbers
+                  value={stars}
+                  className="font-medium text-white"
+                />
+              </div>
             </Link>
           </div>
         </div>
