dce05425c1d9d95da4d2c9552d70afa264404019
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-09-30 18:39:23 +0530

    Removed workaround for deleting YouTube comments.

--- apps/web/utils/youtube/comment.ts
+++ apps/web/utils/youtube/comment.ts
@@ -46,15 +46,15 @@ export async function deleteComment(options: {
   try {
     // Attempt to delete the comment
     //TODO: There is an issue with this from youtube api itself
-    // await youtube.comments.delete({
-    //   id: commentId,
-    // });
+    await youtube.comments.delete({
+      id: commentId,
+    });
 
     //TODO: Remove this work around later
-    await youtube.comments.setModerationStatus({
-      id: [commentId],
-      moderationStatus: "rejected",
-    });
+    // await youtube.comments.setModerationStatus({
+    //   id: [commentId],
+    //   moderationStatus: "rejected",
+    // });
   } catch (error: any) {
     // Handle errors
     console.error("Error deleting comment:", error.message);
